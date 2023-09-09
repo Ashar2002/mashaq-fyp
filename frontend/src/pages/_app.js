e505c01fe72ac648b44245339a227de6284989d5
Ashar2002 <79855121+Ashar2002@users.noreply.github.com>
2023-09-10 03:39:23 +0500

    fonts added back

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,24 +1,25 @@
 import "@/styles/globals.css";
-// import {  Poppins  , Nunito } from "next/font/google";
+import {  Poppins  , Nunito } from "next/font/google";
 
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   variable: "--font-poppins",
-//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-// });
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
-// const nunito = Nunito({
-//   subsets: ["latin"],
-//   variable: "--font-Nunito",
-//   weight: ["200", "300", "400", "700", "900"],
-// });
+const nunito = Nunito({
+  subsets: ["latin"],
+  variable: "--font-Nunito",
+  weight: ["200", "300", "400", "700", "900"],
+});
+
+
 
 export default function App({ Component, pageProps }) {
   return (
-    <main
-    // className={`${poppins.variable} font-poppins ${nunito.variable} `}
-    >
+    <main className={`${poppins.variable} font-poppins ${nunito.variable} `}>
       <Component {...pageProps} />
+   
     </main>
   );
 }
