f5021f89269e62024152d8182793f959985e2faa
Ashar2002 <79855121+Ashar2002@users.noreply.github.com>
2023-09-09 19:41:55 +0500

    banner changes

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <>
+    <div className="relative">
       <Sidebar />
       {/* <Header /> */}
 
@@ -18,7 +18,10 @@ export default function Home() {
       <Search />
       <ImageUpload />
       <BestSelling />
+      <div className="w-20 h-20 bg-black flex justify-center items-center fixed bottom-10 right-10 rounded-full z-[99999]">
+        <span className="text-white">call</span>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
