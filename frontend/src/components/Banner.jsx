cddb578be7370f724a6278e4c3fdb67f05f40d27
Ashar2002 <79855121+Ashar2002@users.noreply.github.com>
2023-09-09 19:01:28 +0500

    banner image changes

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="banner relative bg-cover bg-center bg-no-repeat h-[80vh] ">
+    <div className="banner relative bg-cover bg-center bg-no-repeat xl:h-[80vh] lg:h-[90vh] overflow-hidden">
       <div className="container mx-auto px-4 py-16 flex flex-col gap-5 items-start h-full justify-center">
         <div className="max-w-[500px] lg:mr-auto lg:mx-0 mx-auto">
           <p className="text-brown-0 text-center uppercase tracking-wider font-bold">
@@ -19,15 +19,15 @@ const Banner = () => {
         </div>
       </div>
       <Image
-        className="absolute lg:block hidden top-10 rotate-[342deg] right-[10%] border-8 border-white"
-        src={bannerside1}
+        className="absolute xl:max-w-[300px] lg:max-w-[240px] lg:block hidden bottom-10 rotate-[340deg] right-[20%] border-8 border-white"
+        src={bannerside2}
         width={300}
         height={300}
         alt="bannerside1"
       />
       <Image
-        className="absolute lg:block hidden bottom-10 rotate-[20deg] right-[10%] border-8 border-white"
-        src={bannerside2}
+        className="absolute xl:max-w-[300px] lg:max-w-[240px] lg:block hidden top-0 rotate-[6deg] right-[10%] border-8 border-white"
+        src={bannerside1}
         width={300}
         height={300}
         alt="bannerside1"
